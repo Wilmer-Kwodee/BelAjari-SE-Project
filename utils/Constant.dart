c05846394b44a4cd492a4f3cabc9ec07fd28090c
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-05-25 13:17:04 +0700

    Delete utils directory

--- utils/Constant.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constant {
-  static Color HeroColor = Colors.orange;
-  static Color SecondaryWhite = Colors.white;
-  static Color GreySecondary = Color.fromARGB(255, 230, 230, 230);
-}
